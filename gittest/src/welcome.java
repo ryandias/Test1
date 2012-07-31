d6fecf4a5c04fc3a37b7f9af7d7486c60bdd3b29
Ryan <ryan811@gmail.com>
2012-07-31 11:05:01 +0530

    welcome statement added

---
+++ gittest/src/welcome.java
@@ -0,0 +1,13 @@
+
+public class welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to my Program");
+
+	}
+
+}
