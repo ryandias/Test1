27647268ded38b65dfae881fb396059bcea2f2d8
Ryan <ryan811@gmail.com>
2012-07-31 11:23:08 +0530

    added a trial class

---
+++ gittest/src/trial.java
@@ -0,0 +1,9 @@
+
+public class trial {
+	
+	public void try1()
+	{
+		System.out.println("Hello again");
+	}
+
+}
--- gittest/src/welcome.java
+++ gittest/src/welcome.java
@@ -7,7 +7,8 @@ public class welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to my Program");
-
+		trial t1=new trial();
+		t1.try1();
 	}
 
 }
